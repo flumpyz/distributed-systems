bf49036837fdb726c1822d2836d8640a2d3523a5
flumpyz <natalianata2001@mail.ru>
2022-11-16 22:51:24 +0500

    added redis

--- loader/main.py
+++ loader/main.py
@@ -1,17 +1,45 @@
 import json
 import os
 import sys
+from typing import Optional
 
 import pika
+import redis
 import requests
 
 QUEUE_NAME = 'links'
 
+cache = redis.Redis(host=os.environ['CACHE_HOST'], decode_responses=True)
+
+
+def fetch_status_from_internet(url: str):
+    response = requests.get(url, timeout=10)
+    status = response.status_code
+    return status
+
+
+def get_from_cache(cache_key: str) -> Optional[int]:
+    value = cache.get(cache_key)
+    return value
+
+
+def set_cache(cache_key: str, status_code: int) -> None:
+    cache.set(cache_key, status_code, ex=10)
+
+
+def get_status(url: str) -> int:
+    cache_key = f"url-{url}"
+    status_code = get_from_cache(cache_key)
+    if status_code is None:
+        status_code = fetch_status_from_internet(url)
+        set_cache(cache_key, status_code)
+
+    return status_code
+
 
 def handle_message(ch, method, properties, body):
     link_json = json.loads(body.decode('utf-8'))
-    response = requests.get(link_json['url'], timeout=10)
-    status = response.status_code
+    status = get_status(link_json['url'])
 
     web_url = f'{os.environ["WEB_BASE_URL"]}/links/{link_json["id"]}'
     web_request_body = {
